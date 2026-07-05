d32a89adb525aa9a66794aa2f631d25101abc81a
qq18380389890-glitch <qq18380389890@gmail.com>
2026-07-05 16:57:40 +0800

    perf: lazy load about decorative images

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -8,16 +8,16 @@ export default function AboutSection() {
     <section id="about" className="relative min-h-screen flex flex-col items-center justify-center bg-[#0C0C0C] rounded-t-[40px] sm:rounded-t-[50px] md:rounded-t-[60px] -mt-10 sm:-mt-12 md:-mt-14 z-10 px-5 sm:px-8 md:px-10 pt-32 pb-48 overflow-hidden">
       {/* Decorative Icons */}
       <FadeIn delay={0.1} x={-80} y={0} duration={0.9} className="absolute top-[4%] left-[1%] sm:left-[2%] md:left-[4%] w-[120px] sm:w-[160px] md:w-[210px] z-0">
-        <img src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/moon_icon.11395d36.png" alt="" referrerPolicy="no-referrer" className="w-full h-auto" />
+        <img src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/moon_icon.11395d36.png" alt="" referrerPolicy="no-referrer" loading="lazy" decoding="async" className="w-full h-auto" />
       </FadeIn>
       <FadeIn delay={0.25} x={-80} y={0} duration={0.9} className="absolute bottom-[8%] left-[3%] sm:left-[6%] md:left-[10%] w-[100px] sm:w-[140px] md:w-[180px] z-0">
-        <img src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/p59_1.4659672e.png" alt="" referrerPolicy="no-referrer" className="w-full h-auto" />
+        <img src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/p59_1.4659672e.png" alt="" referrerPolicy="no-referrer" loading="lazy" decoding="async" className="w-full h-auto" />
       </FadeIn>
       <FadeIn delay={0.15} x={80} y={0} duration={0.9} className="absolute top-[4%] right-[1%] sm:right-[2%] md:right-[4%] w-[120px] sm:w-[160px] md:w-[210px] z-0">
-        <img src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/lego_icon-1.703bb594.png" alt="" referrerPolicy="no-referrer" className="w-full h-auto" />
+        <img src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/lego_icon-1.703bb594.png" alt="" referrerPolicy="no-referrer" loading="lazy" decoding="async" className="w-full h-auto" />
       </FadeIn>
       <FadeIn delay={0.3} x={80} y={0} duration={0.9} className="absolute bottom-[8%] right-[3%] sm:right-[6%] md:right-[10%] w-[130px] sm:w-[170px] md:w-[220px] z-0">
-        <img src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/Group_134-1.2e04f3ce.png" alt="" referrerPolicy="no-referrer" className="w-full h-auto" />
+        <img src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/Group_134-1.2e04f3ce.png" alt="" referrerPolicy="no-referrer" loading="lazy" decoding="async" className="w-full h-auto" />
       </FadeIn>
 
       <div className="relative z-10 flex flex-col items-center max-w-5xl text-center">
